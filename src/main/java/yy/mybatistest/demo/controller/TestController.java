f4df1887dcc0a7ae95d691d01f181ccf8730cc3f
yuyue19 <1063219646@qq.com>
2021-03-10 18:53:39 +0800

    封装请求返回泛型

--- src/main/java/yy/mybatistest/demo/controller/TestController.java
+++ src/main/java/yy/mybatistest/demo/controller/TestController.java
@@ -1,10 +1,13 @@
 package yy.mybatistest.demo.controller;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import yy.mybatistest.demo.Result;
 import yy.mybatistest.demo.mapper.UserMapper;
+import yy.mybatistest.demo.pojo.User;
 
 @Controller
 public class TestController {
@@ -14,7 +17,7 @@ public class TestController {
 
     @ResponseBody
     @GetMapping("/test")
-    public String getAge(){
-        return userMapper.selectByPrimaryKey(1).getName();
+    public Result<User> getAge(){
+        return Result.success(userMapper.selectByPrimaryKey(1));
     }
 }
